0851070278e3173e74e127e0824d3edf1d31d6aa
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-04-10 15:32:39 +0200

    feat(philosophers.h): Ajout bibliotheque

---
+++ includes/philosophers.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/10 15:22:13 by mg                #+#    #+#             */
+/*   Updated: 2025/04/10 15:30:46 by mg               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSPHERS_H
+# define PHILOSPHERS_H
+
+
+#include <stdio.h>      // pour printf
+#include <stdlib.h>     // pour malloc, free
+#include <string.h>     // pour memset
+#include <unistd.h>     // pour write, usleep
+#include <sys/time.h>   // pour gettimeofday
+#include <pthread.h>    // pour toutes les fonctions pthread (threads & mutex)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
