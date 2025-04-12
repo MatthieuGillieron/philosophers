f8a0c162a9001840d600f5610ebf59379de50aad
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-04-13 00:50:09 +0200

    test(philosophers.h): Test des colors

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 15:23:12 by mg                #+#    #+#             */
-/*   Updated: 2025/04/12 19:30:04 by mg               ###   ########.fr       */
+/*   Updated: 2025/04/13 00:47:23 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ int main(int ac, char **av)
 {
     if (ac == 5 || ac == 6)
     {
-
+            //ok
     }
     else
     {
-        exit_error("Bad input.. : \n" GREEN"tape ./philo 5 800 200 200 [5]"RST);
+        exit_error(RED"Bad input.. : \n"RST GREEN"tape ./philo 5 800 200 200 [5]"RST);
     }
 }
---
+++ src/test.c
@@ -0,0 +1,7 @@
+#include "../includes/philosophers.h"
+
+
+int main()
+{
+    printf(GREEN"test les colors et les input \n"RST RED"2eme test\n"RST);
+}
