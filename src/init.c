6239a7e318d368dd7ad51fe68302fc93ace64670
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-04-14 10:05:25 +0200

    init(init.c): New file for data

--- src/init.c
+++ src/init.c
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/10 15:23:12 by mg                #+#    #+#             */
-/*   Updated: 2025/04/14 09:58:34 by mg               ###   ########.fr       */
+/*   Created: 2025/04/14 10:03:45 by mg                #+#    #+#             */
+/*   Updated: 2025/04/14 10:05:21 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
-
-/*
-        input : ./philo 5 800 200 200 [5]
-    nbr_philo, time_die time_eat time_sleep [must_eat]
-*/
-
-int main(int ac, char **av)
-{
-    t_table table;
-    
-    if (ac == 5 || ac == 6)
-    {
-            parse_input(&table, av);
-    }
-    else
-    {
-        exit_error(RED"Bad input.. : \n"RST GREEN"tape ./philo 5 800 200 200 [5]"RST);
-    }
-}
