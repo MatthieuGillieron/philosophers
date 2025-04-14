c4c8899c8a6d3264cc52bc3d03afa4820ffbb70a
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-04-14 15:17:51 +0200

    feat(utils): FT handle_thread_error + FT safe_thread_handle

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 15:23:08 by mg                #+#    #+#             */
-/*   Updated: 2025/04/14 14:01:25 by mg               ###   ########.fr       */
+/*   Updated: 2025/04/14 15:16:22 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,19 +45,19 @@ static void handle_mtx_error(int status, t_opcode opcode)
 {
     if (0 == status)
         return;
-        if (EINVAL == status && (LOCK == opcode || UNLOCK == opcode
-            || DESTROY == opcode))
-            exit_error(RED"Value specified by mutex is broken.."RST);
-        else if (EINVAL == status && INIT == opcode)
-            exit_error(RED"Value specified by attr not valid.."RST);
-        else if (EDEADLK == status)
-            exit_error(RED"Deadlock would occur if the thread wainting for mutex.."RST);
-        else if (EPERM == status)
-            exit_error(RED"Current thread does not hold a lock on mutex.."RST);
-        else if (ENOMEM == status)
-            exit_error(RED"Process cant allocate memory to create another mutex.."RST);
-        else if (EBUSY == status)
-            exit_error(RED"Mutex locked.."RST);
+    if (EINVAL == status && (LOCK == opcode || UNLOCK == opcode
+        || DESTROY == opcode))
+        exit_error(RED"Value specified by mutex is broken.."RST);
+    else if (EINVAL == status && INIT == opcode)
+        exit_error(RED"Value specified by attr not valid.."RST);
+    else if (EDEADLK == status)
+        exit_error(RED"Deadlock would occur if the thread wainting for mutex.."RST);
+    else if (EPERM == status)
+        exit_error(RED"Current thread does not hold a lock on mutex.."RST);
+    else if (ENOMEM == status)
+        exit_error(RED"Process cant allocate memory to create another mutex.."RST);
+    else if (EBUSY == status)
+        exit_error(RED"Mutex locked.."RST);
 }
 
 
@@ -73,4 +73,35 @@ void    safe_mutex_handle(t_mtx *mutex, t_opcode opcode)
         handle_mtx_error(pthread_mutex_destroy(mutex), opcode);
     else
         exit_error(RED"Wrong opcode for mutex.."RST);
+}
+
+static void handle_thread_error(int status, t_opcode opcode)
+{
+    if (0 == status)
+        return;
+    if (EAGAIN == status)
+        exit_error(RED"No ressource for create other thread.."RST);
+    else if (EPERM == status)
+        exit_error(RED"Dont have permission.."RST);
+    else if (EINVAL == status && CREATE == opcode)
+        exit_error(RED"Value specified by attr not is not valid.."RST);
+    else if (EINVAL == status && (JOIN == opcode || DETACH == opcode))
+        exit_error(RED"Value specified is not joinable.."RST);
+    else if (ESRCH == status)
+        exit_error(RED"No correspondance, specified thread ID.."RST);
+    else if (EDEADLK == status)
+        exit_error(RED"Deadlock detected.."RST);
+}
+
+
+void    safe_thread_handle(pthread_t *thread, void *(*foo)(void *), void *data, t_opcode opcode)
+{
+    if (CREATE == opcode)
+        handle_thread_error(pthread_create(thread, NULL, foo, data), opcode);
+    else if (JOIN == opcode)
+        handle_thread_error(pthread_join(thread, NULL), opcode);
+    else if (DETACH == opcode)
+        handle_thread_error(pthread_detach(*thread), opcode);
+    else
+        exit_error(RED"Wrong opcode for thread..\n use: <CREATE> <JOIN> <DETACH>"RST);
 }
