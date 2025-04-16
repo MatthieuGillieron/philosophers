fea9686482da6d9da8fa86f62ac3e7c0eb6b9516
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-04-16 11:13:57 +0200

    feat(synchro): Add FT better usleep ->remake usleep more precise

--- src/synchro.c
+++ src/synchro.c
@@ -6,9 +6,14 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 14:56:13 by mg                #+#    #+#             */
-/*   Updated: 2025/04/15 14:56:23 by mg               ###   ########.fr       */
+/*   Updated: 2025/04/15 14:59:43 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
+void    wait_thread(t_table *table)
+{
+    while (!get_bool(&table->table_mtx, &table->all_thread))
+        ;
+}
